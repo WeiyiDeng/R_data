be3fd7e1f0b24856c191efd6ee480effab785625
WeiyiDeng <weiyiwdeng@gmail.com>
2016-10-18 12:34:05 +0200

    API trial with Twitter

--- DSTB/cleaning_data/week2.R
+++ DSTB/cleaning_data/week2.R
@@ -62,7 +62,66 @@ str(myobj)
 
 # myobj turns out to be a matrix with char elements
 link_extend <- myobj[2,]
-link_for_scrap <- paste("https://www.thuisbezorgd.nl", link_extend, sep = "")
+link_for_scrap <- paste("http://www.thuisbezorgd.nl", link_extend, sep = "")
+
+# 
+web_scrapped <- vector("list", length = length(link_for_scrap))                   # create a list       check list [][]?
+str(web_scrapped)
+for (k in 1:length(link_for_scrap)) {
+  food_url <- link_for_scrap[k]
+  html_food = GET(food_url)
+  food_content = content(html_food,as="text")
+  web_scrapped[[k]] <- htmlParse(food_content,asText=TRUE)
+}
+
+## w: check how to write trycatch !!
+# http://stackoverflow.com/questions/12193779/how-to-write-trycatch-in-r
+
+xpathSApply(web_scrapped[[3]], "//label[@class='pulldown_transparent']", xmlValue)
+xpathSApply(web_scrapped[[3]], "//div[@class='button_add']", xmlValue)
+
+## check on getting reviews
+# https://www.thuisbezorgd.nl/en/reviews-johnnys-burger-company-rotterdam/3
+
+save(web_scrapped, file = "food_scrap.RData")
+rm(list=setdiff(ls(), "web_scrapped"))
 
 
+
+## Twitter API
+library(httr)
+myapp = oauth_app("twitter",
+                  key="xn1xWsDrDPBDwTDckdTNMTHtt",secret="L5BMk4Agdoj86CJE29sq9mdnVaOlUpl0faamsqL2vdctc1veHC")
+sig = sign_oauth1.0(myapp,
+                    token = "2201560691-wqSaWaOXFPXgH20i5tZDBRelIOozBS3g2LJOmCP",
+                    token_secret = "UD8dcl0re4ozofoV6DWtSAcjuwduOPu7lddVy47ImIuyI")
+homeTL = GET("https://api.twitter.com/1.1/friends/list.json?count=120&screen_name=KimKardashian", sig)
+# see https://dev.twitter.com/rest/reference/get/friends/list
+
+json1 = content(homeTL)
+
+library(jsonlite)
+json2 = jsonlite::fromJSON(toJSON(json1))
+json2[[1]][[3]]                                       # get a list of people KimKardashian is following
+
+export_json <- jsonlite::toJSON(json2)
+write(export_json, file="KimKardashian_twitter_friends.JSON")                     # export as json file
+
+# see https://dev.twitter.com/rest/reference/get/search/tweets
+# URL encode the query before making the request ! (eg. search query #haiku is encoded into %23haiku in URL)
+# see https://dev.twitter.com/rest/public/search
+homeTL = GET("https://api.twitter.com/1.1/search/tweets.json?q=%23BlackLivesMatter&count=50", sig)     # get 50 recent tweets with hashtag #BlackLivesMatter
+json1 = content(homeTL)
+json2 = jsonlite::fromJSON(toJSON(json1))
+export_json <- jsonlite::toJSON(json2)
+write(export_json, file="BLM_hashtag.JSON")                     # export as json file
+
+names(json2)
+names(json2$statuses)
+json2$statuses$text
+
+json2$statuses$id                                               # view ids of the tweets
+
+# need to write loops with max_id or since_id to page through results?
+# see https://dev.twitter.com/rest/public/timelines
 
